a5488b9ffde774731e12e0db65385ca0e116ba63
Dmitriy Zverev <zver.lockon@mail.ru>
2017-07-21 12:35:00 +0300

    Add feature to serve static files for "bin/run" script

--- tools/run.js
+++ tools/run.js
@@ -1,16 +1,24 @@
+import mount from 'koa-mount';
+import serve from 'koa-static';
+
+import {PUBLIC_URL, PUBLIC_DIR} from '../config';
 import {runServer} from './utils';
 import build from './build';
 
-function run() {
+function run(middlewares) {
   const assets = require('../build/assets.json');
   const app = require('../build/server.js').default(assets);
-  runServer(() => Promise.resolve(app));
+  runServer(() => Promise.resolve(app), middlewares);
 }
 
-export default function (isBuild) {
-  if (isBuild) {
-    build(run);
+export default function ({runBuild, serveStatic}) {
+  const middlewares = [];
+  if (serveStatic) {
+    middlewares.push(mount(PUBLIC_URL, serve(PUBLIC_DIR)));
+  }
+  if (runBuild) {
+    build(() => run(middlewares));
   } else {
-    run();
+    run(middlewares);
   }
 }

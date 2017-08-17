d65d5d0ebba714e1c63f530687083feb8e7661f0
Dmitriy Zverev <zver.lockon@mail.ru>
2017-08-17 22:24:30 +0300

    Optimize client Root component
    Wrap the Root component in AppContainer from "react-hot-loader"
    only in development environment.

--- client/components/Root.js
+++ client/components/Root.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
-import {AppContainer} from 'react-hot-loader';
 
 import App from 'components/App';
 
@@ -10,15 +9,18 @@ import App from 'components/App';
  * The root client component.
  */
 function Root({store, routes}) {
-  return (
-    <AppContainer>
-      <Provider store={store}>
-        <BrowserRouter>
-          <App routes={routes}/>
-        </BrowserRouter>
-      </Provider>
-    </AppContainer>
+  const component = (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App routes={routes}/>
+      </BrowserRouter>
+    </Provider>
   );
+  if (process.env.NODE_ENV === 'development') {
+    const {AppContainer} = require('react-hot-loader');
+    return <AppContainer>{component}</AppContainer>;
+  }
+  return component;
 }
 
 Root.propTypes = {
